6139eacb455690143904e6266d71d2e195589b4b
viktor.kochetov-arkhipov <viktor.kochetov-arkhipov@sms-a.ru>
2024-01-01 19:50:13 +0300

    2-12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit, ChangeDetectorRef,
+  Component,
+  ViewChild,
+  ViewContainerRef
+} from '@angular/core';
 import { User } from './auth-form-module/models/auth-form.interface';
+import { AuthFormComponent } from './auth-form-module/components/auth-form/auth-form.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('entry', {read: ViewContainerRef}) entry: ViewContainerRef;
+
   rememberMe: boolean = false;
+
+  constructor(
+    private cdr: ChangeDetectorRef
+  ) {
+  }
+
+  ngAfterViewInit(): void {
+    const component = this.entry.createComponent(AuthFormComponent);
+    this.cdr.detectChanges();
+  }
 
   rememberUser(remember: boolean) {
     this.rememberMe = remember;
