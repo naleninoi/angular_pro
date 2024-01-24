5dd9aaf26257008c6f31a341ec09eb16183b05a8
viktor.kochetov-arkhipov <viktor.kochetov-arkhipov@sms-a.ru>
2024-01-24 18:32:16 +0300

    3-24

--- src/app/credit-card-module/directives/credit-card.directive.ts
+++ src/app/credit-card-module/directives/credit-card.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appCreditCard]'
@@ -8,5 +8,22 @@ export class CreditCardDirective {
   constructor(
     private element: ElementRef
   ) { }
+
+  @HostListener('input', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    let trimmed = input.value.replace(/\s+/g, '');
+    if (trimmed.length > 16) {
+      trimmed = trimmed.substring(0, 16);
+    }
+
+    let numbers = [];
+    for (let i = 0; i < trimmed.length; i += 4) {
+      const part = trimmed.substring(i, 4)
+      numbers.push(part);
+    }
+
+    input.value = numbers.join(' ');
+  }
 
 }
