39afcfa6b682e722d00e6e8c2eae73a90ca7fd13
viktor.kochetov-arkhipov <viktor.kochetov-arkhipov@sms-a.ru>
2024-01-31 15:25:20 +0300

    6-49

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -29,5 +29,11 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   constructor(private router: Router) {}
-  ngOnInit() {}
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        console.log(event);
+      }
+    });
+  }
 }
